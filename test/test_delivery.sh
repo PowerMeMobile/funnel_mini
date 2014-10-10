dc6e96d899f47a711edf390d204998fe7bb40093
Dmitry Klionsky <dm.klionsky@gmail.com>
2014-10-10 18:07:32 +0300

    Add delivery and encoding tests

---
+++ test/test_delivery.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+#
+# You need SMPPSim (http://www.seleniumsoftware.com/user-guide.htm),
+# smppsink (https://github.com/PowerMeMobile/smppsink) and
+# smppload (https://github.com/PowerMeMobile/smppload) to run these tests,
+# as well as a correctly configured customer and its coverage map.
+# See https://github.com/PowerMeMobile/alley-setup/ for more details.
+#
+
+HOST=localhost
+PORT=2775
+SYSTEM_TYPE=''
+SYSTEM_ID=user
+PASSWORD=password
+SRC_ADDR=375296660001
+DST_ADDR=999296543210
+
+SMPPSIM_SERVER='http://127.0.0.1:8071'
+
+EXIT=0
+
+function check() {
+    local command="$1"
+    local delivery="$2"
+    local invert="$3"
+    local pattern="$4"
+    local encoding="${5-3}" # default latin1
+
+    case "$delivery" in
+        !dlr) delivery_flag=false;;
+        dlr) delivery_flag=true
+    esac
+
+    case "$invert" in
+        w/o) invert_match="--invert-match";;
+        with) invert_match=""
+    esac
+
+    echo -n "$SRC_ADDR;$DST_ADDR;$command;$delivery_flag;$encoding" |
+    smppload --host=$HOST --port=$PORT --system_type=$SYSTEM_TYPE --system_id=$SYSTEM_ID --password=$PASSWORD \
+        --file - -vv | grep $invert_match "$pattern" > /dev/null
+
+    if [[ "$?" != 0 ]]; then
+        echo -e "$command\t$delivery\t\e[31mFAIL\e[0m"
+        EXIT=1
+    else
+        echo -e "$command\t$delivery\t\e[32mOK\e[0m"
+    fi
+}
+
+function send_incoming_via_smppsim() {
+    local src="$1"
+    local dst="$2"
+    local msg="$3"
+
+    local url="$SMPPSIM_SERVER/inject_mo?\
+short_message=$msg&\
+source_addr=$src&\
+destination_addr=$dst&\
+source_addr_ton=1&source_addr_npi=0&dest_addr_ton=1&dest_addr_npi=1"
+
+    curl -s "$url" > /dev/null
+}
+
+echo "#"
+echo "# Check delivery statuses"
+echo "#"
+
+# standard
+check "receipt:enroute"       dlr with "stat:ENROUTE"
+check "receipt:delivered"     dlr with "stat:DELIVRD"
+check "receipt:expired"       dlr with "stat:EXPIRED"
+check "receipt:deleted"       dlr with "stat:DELETED"
+check "receipt:undeliverable" dlr with "stat:UNDELIV"
+check "receipt:accepted"      dlr with "stat:ACCEPTD"
+check "receipt:unknown"       dlr with "stat:UNKNOWN"
+check "receipt:rejected"      dlr with "stat:REJECTD"
+
+# non-standard
+check "receipt:unrecognized"  dlr with "stat:UNRECOG"
+check "receipt:BADSTATUS"     dlr with "stat:UNRECOG"
+
+# check errorneous submit, but with dlr
+check "submit:1" dlr with "stat:REJECTD"
+
+echo "#"
+echo "# Check incomings"
+echo "#"
+
+B=$RANDOM; send_incoming_via_smppsim $DST_ADDR $SRC_ADDR $B; check "dummy" dlr with "{short_message,\"$B\"}"
+
+exit $EXIT
---
+++ test/test_encoding.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+
+#
+# You need SMPPSim (http://www.seleniumsoftware.com/user-guide.htm) and
+# smppload (https://github.com/PowerMeMobile/smppload) to run these tests,
+# as well as a correctly configured customer and its coverage map.
+# See https://github.com/PowerMeMobile/alley-setup/ for more details.
+#
+# To make the tests run more smoothly use:
+# funnel_conf:set(batch_max_wait, 1000).
+# application:set_env(k_handlers, receipt_retry_timeout, 10).
+#
+
+HOST=localhost
+PORT=2775
+SYSTEM_TYPE=''
+SYSTEM_ID=user
+PASSWORD=password
+SRC_ADDR=375296660001
+DST_ADDR=375296543210
+
+EXIT=0
+
+function check() {
+    local body="$1"
+    local encoding="$2"
+    local parts="$3"
+
+    echo -en "length: ${#body}\tparts: $parts == "
+
+    lines=`smppload --host=$HOST --port=$PORT --system_type=$SYSTEM_TYPE --system_id=$SYSTEM_ID --password=$PASSWORD \
+        --source=$SRC_ADDR --destination=$DST_ADDR --body="$body" --delivery --thread_count=5 --data_coding=$encoding -vv | egrep "Receipt.*DELIVRD" | wc -l`
+
+    if [[ "$?" == 0 && $lines == $parts ]]; then
+        echo -e "$lines\t\e[32mOK\e[0m"
+    else
+        echo -e "$lines\t\e[31mFAIL\e[0m"
+        EXIT=1
+    fi
+}
+
+echo "#"
+echo "# Check encoding 0 (gsm0338 7-bit)"
+echo "#"
+
+# 160 symbols - 1 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJ\
+"
+check "$BODY" 0 1
+
+# 161 symbols - 2 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJK\
+"
+check "$BODY" 0 2
+
+# 306 symbols - 2 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345\
+"
+check "$BODY" 0 2
+
+# 307 symbols - 3 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456\
+"
+check "$BODY" 0 3
+
+# 459 symbols - 3 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxy\
+"
+check "$BODY" 0 3
+
+# 460 symbols - 4 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz\
+"
+check "$BODY" 0 4
+
+echo "#"
+echo "# Check encoding 1 (ascii 7-bit)"
+echo "#"
+
+# 160 symbols - 1 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJ\
+"
+check "$BODY" 1 1
+
+# 161 symbols - 2 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJK\
+"
+check "$BODY" 1 2
+
+# 306 symbols - 2 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345\
+"
+check "$BODY" 1 2
+
+# 307 symbols - 3 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456\
+"
+check "$BODY" 1 3
+
+# 459 symbols - 3 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxy\
+"
+check "$BODY" 1 3
+
+# 460 symbols - 4 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz\
+"
+check "$BODY" 1 4
+
+echo "#"
+echo "# Check encoding 3 (latin1 8-bit)"
+echo "#"
+
+# 140 symbols - 1 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnop\
+"
+check "$BODY" 3 1
+
+# 141 symbols - 2 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopq\
+"
+check "$BODY" 3 2
+
+# 268 symbols - 2 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrst\
+"
+check "$BODY" 3 2
+
+# 269 symbols - 3 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstu\
+"
+check "$BODY" 3 3
+
+# 402 symbols - 3 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCD\
+"
+check "$BODY" 3 3
+
+# 403 symbols - 4 mgs
+BODY="\
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDE\
+"
+check "$BODY" 3 4
+
+echo "#"
+echo "# Check encoding 8 (ucs2 - 16-bit)"
+echo "#"
+
+# 70 symbols - 1 msg
+BODY="\
+абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123\
+"
+check "$BODY" 8 1
+
+# 71 symbols - 2 msgs
+BODY="\
+абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ01234\
+"
+check "$BODY" 8 2
+
+# 134 symbols - 2 msgs
+BODY="\
+абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧ\
+"
+check "$BODY" 8 2
+
+# 135 symbols - 3 msgs
+BODY="\
+абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШ\
+"
+check "$BODY" 8 3
+
+# 201 symbols - 3 msgs
+BODY="\
+абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНО\
+"
+check "$BODY" 8 3
+
+# 202 symbols - 4 msgs
+BODY="\
+абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОП\
+"
+check "$BODY" 8 4
+
+exit $EXIT
